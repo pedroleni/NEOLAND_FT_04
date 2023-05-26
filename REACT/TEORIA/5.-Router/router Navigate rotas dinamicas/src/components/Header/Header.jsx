bc35ade0c3eaee4ec83a0f6e9832e7013af6b162
pedroleni <pedroleridanieto@hotmail.com>
2023-05-26 13:36:39 +0200

    cambios en el header para el total del button te vaya a la ruta

--- REACT/TEORIA/5.-Router/router Navigate rotas dinamicas/src/components/Header/Header.jsx
+++ REACT/TEORIA/5.-Router/router Navigate rotas dinamicas/src/components/Header/Header.jsx
@@ -3,18 +3,22 @@ import "./Header.css";
 import { NavLink } from "react-router-dom";
 
 const Header = () => {
+  // el navlink tiene que ir como padre del button para hacer al button totalmente clickable y navegable
+  // si meto dentro del buttton el navlink solo me navega cuando le doy al texto
   return (
     <header>
       <nav>
-        <button>
-          <NavLink to="/">Home</NavLink>
-        </button>
-        <button>
-          <NavLink to="/gallery">Gallery</NavLink>
-        </button>
-        <button>
-          <NavLink to="/about">About</NavLink>
-        </button>
+        <nav>
+          <NavLink to="/">
+            <button>Home</button>
+          </NavLink>
+          <NavLink to="/gallery">
+            <button>Gallery</button>
+          </NavLink>
+          <NavLink to="/about">
+            <button>About</button>
+          </NavLink>
+        </nav>
       </nav>
     </header>
   );
