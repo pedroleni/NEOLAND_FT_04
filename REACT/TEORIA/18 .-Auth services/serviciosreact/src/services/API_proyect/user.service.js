cb478924156954c04b5a696bbf4297a292393f78
pedroleni <pedroleridanieto@hotmail.com>
2023-06-03 20:38:13 +0200

    multipart en el service

--- REACT/TEORIA/18 .-Auth services/serviciosreact/src/services/API_proyect/user.service.js
+++ REACT/TEORIA/18 .-Auth services/serviciosreact/src/services/API_proyect/user.service.js
@@ -1,7 +1,9 @@
 import { API } from "./service.config";
 
 export const registerUser = async (formData) => {
-  return API.post("/users/register", formData)
+  return API.post("/users/register", formData, {
+    headers: { "Content-Type": "multipart/form-data" },
+  })
     .then((res) => res)
     .catch((error) => {
       return error;
