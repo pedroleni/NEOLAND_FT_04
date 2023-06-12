fa11a06e499ba329fe91f4618957401141d9eeba
pedroleni <pedroleridanieto@hotmail.com>
2023-06-12 15:49:29 +0200

    cambio en el protected

--- REACT/TEORIA/18 .-Auth services/serviciosreact/src/components/Protected.jsx
+++ REACT/TEORIA/18 .-Auth services/serviciosreact/src/components/Protected.jsx
@@ -4,10 +4,10 @@ import { useAuth } from "../contexts/authContext";
 export const Protected = ({ children }) => {
   // tendremos que traernos el user del contexto
 
-  const { user } = useAuth();
+  const { user, logout } = useAuth();
 
   if (user == null || user?.check == false) {
-    return <Navigate to="/login" />;
+    logout();
   }
 
   return children;
